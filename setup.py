fa67d0ba1043040d426b365da878a92e26562087
shravani Kusare <shravanikusare@gmail.com>
2026-01-13 16:46:28 +0530

    SetUp

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    """Returns a list of requirements from a file"""
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Shravani",
+    author_email="shravanikusare@gmail.com",
+    packages=find_packages(),   
+    install_requires=get_requirements("requirements.txt"),
+)
